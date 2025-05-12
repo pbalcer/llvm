76c24d9da18aeb4d025382116cc0de24093d61ac
Piotr Balcer <piotr.balcer@intel.com>
2025-05-12 14:58:37 +0000

    [UR][L0v2] basic host tasks implementation

--- unified-runtime/source/adapters/cuda/ur_interface_loader.cpp
+++ unified-runtime/source/adapters/cuda/ur_interface_loader.cpp
@@ -441,6 +441,7 @@ UR_DLLEXPORT ur_result_t UR_APICALL urGetEnqueueExpProcAddrTable(
   pDdiTable->pfnUSMHostAllocExp = urEnqueueUSMHostAllocExp;
   pDdiTable->pfnUSMFreeExp = urEnqueueUSMFreeExp;
   pDdiTable->pfnCommandBufferExp = urEnqueueCommandBufferExp;
+  pDdiTable->pfnHostTaskExp = urEnqueueHostTaskExp;
 
   return UR_RESULT_SUCCESS;
 }
